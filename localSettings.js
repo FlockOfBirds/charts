bed4198fd4d9390e2da5ac5c9b56bd6bd9ded4a7
Joseph D <j_seremba@live.com>
2018-03-05 11:55:55 +0300

    Setup end-to-end tests

---
+++ localSettings.js
@@ -0,0 +1,7 @@
+exports.settings = {
+    appName: "charts102",
+    key: "fff881e3-0785-4c0c-b868-55966203523f",
+    password: "e7Q9!9eP",
+    projectId: "649a8fda-c4a6-4303-9d8e-31402bc2840e",
+    user: "ci@flockofbirds.org"
+};
